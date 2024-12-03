2b48c1244cc91636284b8aa9e24af4a47688107f
Ruhan130 <ruhank170@gmail.com>
2024-12-03 15:09:04 +0500

    config

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
                 FocusManager.instance.primaryFocus!.unfocus();
               }
             },
-            child: const DynamicTextFieldScreen(),
+            child:  DynamicTextFieldScreen(),
           )),
     );
   }
