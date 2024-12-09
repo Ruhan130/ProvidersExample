b54a32fa07accd3618425425868c6862d6c9b3a2
Ruhan130 <ruhank170@gmail.com>
2024-12-09 11:07:31 +0500

    Starting working on FormField

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
                 FocusManager.instance.primaryFocus!.unfocus();
               }
             },
-            child:  Imagpickerview(),
+            child:  DynamicTextFieldScreen(),
           )),
     );
   }
